d5a0f28b7132f49700cc7b0ea1b81a576a10d49b
MellitiYassine <yassine.melliti@etudiant-fst.utm.tn>
2025-02-02 22:32:22 +0100

    feat: update launch template and ec2 role

--- asg.tf
+++ asg.tf
@@ -1,20 +1,89 @@
-# Launch Template
+# IAM Role for EC2 Instances
+resource "aws_iam_role" "ecommerce_ec2_role" {
+  name = "ecommerce-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for S3 Access
+resource "aws_iam_policy" "ecommerce_s3_access_policy" {
+  name        = "ecommerce-s3-access-policy"
+  description = "Policy to allow EC2 instances to access the S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect = "Allow"
+        Resource = [
+          aws_s3_bucket.ecommerce_jar_bucket.arn,
+          "${aws_s3_bucket.ecommerce_jar_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach IAM Policy to IAM Role
+resource "aws_iam_role_policy_attachment" "ecommerce_s3_access_attachment" {
+  role       = aws_iam_role.ecommerce_ec2_role.name
+  policy_arn = aws_iam_policy.ecommerce_s3_access_policy.arn
+}
+
+# IAM Instance Profile for EC2 Instances
+resource "aws_iam_instance_profile" "ecommerce_ec2_profile" {
+  name = "ecommerce-ec2-profile"
+  role = aws_iam_role.ecommerce_ec2_role.name
+}
+
+# Update Launch Template to Use IAM Role
 resource "aws_launch_template" "ecommerce_ec2_launch_templ" {
   name_prefix   = "ecommerce-ec2-launch-templ"
-  image_id      = data.aws_ami.latest_amazon_linux.id # Dynamically fetch the latest Amazon Linux AMI
+  image_id      = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
-  user_data     = filebase64("user_data.sh")
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecommerce_ec2_profile.name
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              # Install AWS CLI (if not already installed)
+              yum update -y
+              yum install -y aws-cli
+
+              # Download JAR file from S3 bucket
+              aws s3 cp s3://${aws_s3_bucket.ecommerce_jar_bucket.bucket}/be-ecommerce-0.0.1-SNAPSHOT.jar /home/ec2-user/be-ecommerce-0.0.1-SNAPSHOT.jar
+
+              # Run the JAR file
+              java -jar /home/ec2-user/be-ecommerce-0.0.1-SNAPSHOT.jar &
+              EOF
+              )
 
   network_interfaces {
-    associate_public_ip_address = false # No public IP since instances are in private subnets
-    subnet_id                   = aws_subnet.ecommerce_priv_subnet_2.id # Correct subnet reference
-    security_groups             = [aws_security_group.ecommerce_sg_for_ec2.id] # Correct security group reference
+    associate_public_ip_address = false
+    subnet_id                   = aws_subnet.ecommerce_priv_subnet_2.id
+    security_groups             = [aws_security_group.ecommerce_sg_for_ec2.id]
   }
 
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "ecommerce-instance" # Name for the EC2 instances
+      Name = "ecommerce-instance"
     }
   }
 
--- security.tf
+++ security.tf
@@ -1,25 +1,32 @@
+# Data Source to Fetch CloudFront Managed Prefix List
+data "aws_ec2_managed_prefix_list" "cloudfront" {
+  name = "com.amazonaws.global.cloudfront.origin-facing"
+}
+
+# Security Group for Load Balancer
 resource "aws_security_group" "ecommerce_sg_for_elb" {
-  name   = "ecommerce-sg_for_elb"
+  name   = "ecommerce-sg-for-elb"
   vpc_id = aws_vpc.ecommerce_vpc.id
-  
+
+  # Allow HTTP traffic from CloudFront edge locations
   ingress {
-    description      = "Allow http request from anywhere"
+    description      = "Allow HTTP traffic from CloudFront edge locations"
     protocol         = "tcp"
     from_port        = 80
     to_port          = 80
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = [data.aws_ec2_managed_prefix_list.cloudfront.id]
   }
-  
+
+  # Allow HTTPS traffic from CloudFront edge locations
   ingress {
-    description      = "Allow https request from anywhere"
+    description      = "Allow HTTPS traffic from CloudFront edge locations"
     protocol         = "tcp"
     from_port        = 443
     to_port          = 443
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = [data.aws_ec2_managed_prefix_list.cloudfront.id]
   }
 
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -27,6 +34,9 @@ resource "aws_security_group" "ecommerce_sg_for_elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Name = "ecommerce-sg-for-elb"
+  }
 }
 
 resource "aws_security_group" "ecommerce_sg_for_ec2" {
